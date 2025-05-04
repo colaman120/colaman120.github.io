f6ca812b357206058ede56448fbab7685c8acbc0
Jason Hu <colaman120@gmail.com>
2025-05-04 15:23:40 -0600

    Add navbar at top of page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar.js';
+import About from './pages/about.js';
+import Pricing from './pages/pricing.js';
+import Home from './pages/home.js';
+import { Route, Routes } from "react-router-dom";
 
-function App() {
+function App() {  
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          show me da text
-        </p>
-      </header>
+    <>
+    <Navbar/>
+    <div className="container">
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/pricing" element={<Pricing/>} />
+        <Route path="/about" element={<About/>} />
+      </Routes>
     </div>
+    </>
   );
 }
 
