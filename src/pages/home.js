fc59c090ed87b7ac0b8cfeff51c149ee96df6439
Jason Hu <colaman120@gmail.com>
2025-05-19 16:53:34 -0600

    read multiple markdown posts

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,33 +1,3 @@
-import React, { useState, useEffect } from 'react'
-import {createRoot} from 'react-dom/client'
-import Markdown  from "react-markdown"
-
 export default function Home() {
-/*     const file_name = 'testmd.md';
-    const [post, setPost] = useState('');
-
-    useEffect(() => {
-        import(`/C:/Users/jason/colaman120.github.io/src/posts/${file_name}`)
-            .then(res => {
-                fetch(res.default)
-                    .then(res => res.text())
-                    .then(res => setPost(res))
-                    .catch(err => console.log(err));
-            })
-            .catch(err => console.log(err));
-    });
-
-    return (
-        <h1>
-            <Markdown>
-                {post}
-            </Markdown>
-        </h1>
-    ); */
-    const markdown = '#test'
-    return(
-        <h1>
-            <Markdown>{markdown}</Markdown>
-        </h1>    
-    )
+    return <h1>Home</h1>
 }
--- src/pages/media.js
+++ src/pages/media.js
@@ -1,3 +1,51 @@
-export default function Media() {
-    return <h1>Media</h1>
+import React, { Fragment, Component } from 'react';
+import Helmet from 'react-helmet';
+import ReactMarkdown from 'react-markdown';
+
+const importAll = (r) => r.keys().map(r);
+var markdownFiles = importAll(require.context('posts/mediaposts', false, /\.md$/))
+  .reverse();
+var half_length = Math.ceil(markdownFiles.length / 2);    
+markdownFiles = markdownFiles.slice(0,half_length);
+
+class Media extends Component {
+    state = {
+        posts: []
+    }
+
+    async componentDidMount() {
+        const posts = await Promise.all(markdownFiles.map((file) => fetch(file).then((res) => res.text())))
+            .catch((err) => console.error(err));
+        console.log(markdownFiles);
+
+        this.setState((state) => ({ ...state, posts }));
+    }
+
+    
+
+    render() {
+        const { posts } = this.state;
+        return (
+        <Fragment>
+            <Helmet title="The Gang Talks About Sports" />
+            <section className="section">
+                <div>
+                {
+                    posts.map((post, idx) => (
+                        <div className="card" key={idx}>
+                            <div className="card-content">
+                                <div className="content">
+                                    <ReactMarkdown children={post} />
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
+                </div>
+            </section>
+        </Fragment>
+        );
+    }
 }
+
+export default Media;
